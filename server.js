7b1de7b7ab5c601486ebdd41a2b26de2ad4032a9
Kai Wegner <kw@o511.de>
2014-07-09 00:02:01 +0200

    Added tmp to .gitignore and made the first npm release of AutoNode (named autoremoteserver because AutoNode was already taken)

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var framework = require('total.js');
 var http = require('http');
+var port = 1337;
 
-framework.run(http, false, parseInt(process.argv[2]));
+if(process.argv[2] == null && process.env.npm_package_config_port != null)
+  port = parseInt(process.env.npm_package_config_port);
+else if(process.argv[2] != null)
+  port = parseInt(process.argv[2]);
+
+framework.run(http, false, port);
